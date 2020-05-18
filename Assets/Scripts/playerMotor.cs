2168fde7523cd9112fc4523ea9c93267ceff9f44
dxaman <niketdx@iitk.ac.in>
2020-05-18 06:23:15 +0530

    Player and Camera motion added

---
+++ Assets/Scripts/playerMotor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMotor : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 moveVector;
+    float speed = 5.0f;
+
+    // Start is called before the first frame update
+     void Start()
+    {
+        controller=GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector=Vector3.zero;
+
+        moveVector.x=Input.GetAxisRaw("Horizontal")*speed;
+        moveVector.y=Input.GetAxisRaw("Vertical")*speed;
+        moveVector.z=speed;
+        controller.Move(moveVector*Time.deltaTime);
+        
+    }
+}
